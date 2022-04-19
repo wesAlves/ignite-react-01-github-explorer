5cfe7ea19078f6c7a01413460673ce3bde77fe19
Wes <wal2.designer@gmail.com>
2022-04-19 11:33:48 +0100

    added sass to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,46 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV === 'production';
 
-module.exports={
+module.exports = {
     mode: isDevelopment ? 'development' : 'production',
     devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'), // use path resolve to avoid erros based on name patterns from OS like / OR \
-    output:{
+    output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    resolve:{
+    resolve: {
         extensions: ['.js', '.jsx']
     },
-    devServer:{
-        static:{
+    devServer: {
+        static: {
             directory: path.resolve(__dirname, 'public')
         }
         // port: '3332'
     },
-    plugins:[
+    plugins: [
         new HtmlWebPackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
     ],
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
                 test: /\.jsx$/, // $ sign means that this occurency must be at the end.
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+                test: /\.s[ac]ss$/, // $ sign means that this occurency must be at the end.
+                exclude: /node_modules/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
             }
         ]
     },
