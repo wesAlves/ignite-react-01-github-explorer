1205f3adec0b46b9bfc1c6172d929d39f3d9291f
Wes <wal2.designer@gmail.com>
2022-04-19 10:43:41 +0100

    babel and webpack installed and configured

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports={
+    entry: path.resolve(__dirname, 'src', 'index.jsx'), // use path resolve to avoid erros based on name patterns from OS like / OR \
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve:{
+        extensions: ['.js', '.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test: /\.jsx$/, // $ sign means that this occurency must be at the end.
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
