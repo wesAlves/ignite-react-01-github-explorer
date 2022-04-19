1ad25c7d5b482d68bbc12c9449c6598af63d7be5
Wes <wal2.designer@gmail.com>
2022-04-19 10:52:45 +0100

    base config react

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
+const { runtime } = require("webpack");
+
 module.exports = {
     presets: [
         '@babel/preset-env',
-        '@babel/preset-react',
+        [
+            '@babel/preset-react',
+            {
+                runtime: 'automatic'
+            }]
     ]
 }
--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,6 @@ module.exports={
                 use: 'babel-loader'
             }
         ]
-    }
+    },
+    mode: 'development'
 }
